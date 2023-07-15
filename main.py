ded616bd00cac15dd1f58f49e2f3ebe2a573d361
KyrylenkoMaks <makss.kyrylenko@gmail.com>
2023-07-16 01:08:33 +0300

    key getting modify

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import random
+
 import telebot
 from telebot import types
 
 hint_button = types.InlineKeyboardButton(text='Взяти підказку', callback_data="takehint")
 markup_inline = types.InlineKeyboardMarkup()
-
-hang_bot = telebot.TeleBot('TOKEN')
+with open("telegramkey.txt", 'r') as file:
+    hang_bot = telebot.TeleBot(file.read())
 current_game = None
-wordlist = ['удача', 'кілт', 'буран', 'будинок', 'розмарин', 'окуляри', 'фольга', 'батарея', 'піцца', 'вежа', 'цукерка', 'гаманець', 'дзига', 'танго']
+wordlist = ['удача', 'кілт', 'буран', 'будинок', 'розмарин', 'окуляри', 'фольга', 'батарея', 'піцца', 'вежа', 'цукерка',
+            'гаманець', 'дзига', 'танго']
 
 
 class HangmanGame:
@@ -28,16 +30,20 @@ class HangmanGame:
             self.masked_word = ''.join(char if char.lower() in self.guessed_letters else "*" for char in self.word)
             if self.masked_word == self.word:
                 current_game = None
-                hang_bot.send_message(message.chat.id, f'Молодець! Гру завершено!\nПравильне слово: {self.word}\nЯкщо хочеш зіграти ще, відправляй /play')
+                hang_bot.send_message(message.chat.id,
+                                      f'Молодець! Гру завершено!\nПравильне слово: {self.word}\nЯкщо хочеш зіграти ще, відправляй /play')
             elif letter in self.word:
-                hang_bot.send_message(message.chat.id, f'Гарна здогадка! Тепер слово виглядає так:\n{self.masked_word}\nПродовжуй в тому ж дусі!')
+                hang_bot.send_message(message.chat.id,
+                                      f'Гарна здогадка! Тепер слово виглядає так:\n{self.masked_word}\nПродовжуй в тому ж дусі!')
             else:
                 self.attempts -= 1
                 if self.attempts == 0:
                     current_game = None
-                    hang_bot.send_message(message.chat.id, f'Жаль, але ти програв. Правильне слово: {self.word}.\nЯкщо хочеш зіграти ще - відправляй /play')
+                    hang_bot.send_message(message.chat.id,
+                                          f'Жаль, але ти програв. Правильне слово: {self.word}.\nЯкщо хочеш зіграти ще - відправляй /play')
                 else:
-                    hang_bot.send_message(message.chat.id, f'Такої літери немає в слові. Залишилось помилок - {self.attempts}')
+                    hang_bot.send_message(message.chat.id,
+                                          f'Такої літери немає в слові. Залишилось помилок - {self.attempts}')
 
     def receive_msg(self, message):
         if len(message.text) != 1 or not message.text.isalpha():
@@ -54,6 +60,7 @@ def botgreeting(message):
 У тебе є право на 8 помилок перед тим, як гра закінчиться.
 Коли будеш готовий, відправ /play''')
 
+
 @hang_bot.message_handler(commands=["play"])
 def play(message):
     global markup_inline
@@ -61,15 +68,19 @@ def play(message):
     markup_inline = types.InlineKeyboardMarkup()
     current_game = HangmanGame()
     markup_inline.add(hint_button)
-    hang_bot.send_message(message.chat.id, f'Що ж, давай зіграємо!\nЯ загадав наступне слово:\n{current_game.masked_word}\nСпробуй відгадати!', reply_markup=markup_inline)
+    hang_bot.send_message(message.chat.id,
+                          f'Що ж, давай зіграємо!\nЯ загадав наступне слово:\n{current_game.masked_word}\nСпробуй відгадати!',
+                          reply_markup=markup_inline)
 
-@hang_bot.message_handler(func=lambda message: True,)
+
+@hang_bot.message_handler(func=lambda message: True, )
 def bot_reply(message):
     global current_game
     if current_game == None:
         hang_bot.send_message(message.chat.id, f'Якщо ти хочеш зіграти, відправляй /play')
     else:
         current_game.receive_msg(message)
+
 
 @hang_bot.callback_query_handler(func=lambda call: call.data == "takehint")
 def hint(call):
@@ -86,5 +97,6 @@ def hint(call):
         hang_bot.send_message(call.message.chat.id, f'В цьому слові є літера {hint_letter}')
         current_game.hint_taken = True
         return
+
 
 hang_bot.infinity_polling()
